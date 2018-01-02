2a65dee13d235759acb039ca4e88a46902b80ba4
Alexander Anderson <anderson.1368@osu.edu>
2018-01-02 16:25:48 -0500

    note clean up

--- notes.sh
+++ notes.sh
@@ -1,2 +1,1 @@
-anderson.1368@ODEE186162 /cygdrive/c/Users/anderson.1368/git/jira-backlog-updater
-$ clear; clear; node lib/quickScan.js LSID option start 1516597200000 option points 60 option duration 1209600000 roadmap general roadmap-save release-save
+clear; clear; node lib/quickScan.js LSID option start 1516597200000 option points 60 option duration 1209600000 roadmap general roadmap-save release-save
